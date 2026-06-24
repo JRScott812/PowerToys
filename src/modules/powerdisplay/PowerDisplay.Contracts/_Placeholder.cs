975ff47e596523b39410d00c7365b6da40b52ee0
Yu Leng (from Dev Box) <yuleng@microsoft.com>
2026-06-24 15:13:51 +0800

    feat(powerdisplay-cli): scaffold PowerDisplay.Contracts assembly

---
+++ src/modules/powerdisplay/PowerDisplay.Contracts/_Placeholder.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#pragma warning disable SA1649 // File name should match first type name
+
+namespace PowerDisplay.Contracts;
+
+internal static class AssemblyMarker
+{
+}
